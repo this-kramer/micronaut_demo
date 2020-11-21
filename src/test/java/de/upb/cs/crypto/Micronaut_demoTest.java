20e73c145f6d6ef3a6e919292b2a838632b75d24
Paul Kramer <palkra>
2020-11-21 07:35:05 +0000

    Initial commit

---
+++ src/test/java/de/upb/cs/crypto/Micronaut_demoTest.java
@@ -0,0 +1,21 @@
+package de.upb.cs.crypto;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class Micronaut_demoTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
